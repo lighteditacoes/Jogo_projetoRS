f8087cdc47c5b601b5d14c5eb3114f8bafd96e25
lighteditacoes <rldossantosfreitas@gmail.com>
2025-07-22 18:14:51 -0300

    versão atualizada até 22/07 as 18:14

--- tela_inicial.gd
+++ tela_inicial.gd
@@ -1,9 +1,22 @@
 extends Control
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://jogo/main.tscn")
+
+func _on_jogar_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/main.tscn")
 
 
-func _on_button_4_pressed() -> void:
+func _on_opcoes_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_como_jogar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_creditos_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/creditos.tscn")
+
+
+func _on_sair_pressed() -> void:
+	pass # Replace with function body.
