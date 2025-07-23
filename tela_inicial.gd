120f2f914d566c209af67939919ba83e7911818c
lighteditacoes <rldossantosfreitas@gmail.com>
2025-07-22 21:36:44 -0300

    22/07 - 21:36
    Colisões feitas, menu de configurações e outras correções

--- tela_inicial.gd
+++ tela_inicial.gd
@@ -3,11 +3,11 @@ extends Control
 
 
 func _on_jogar_pressed() -> void:
-	get_tree().change_scene_to_file("res://cenas/main.tscn")
+	get_tree().change_scene_to_file("res://cenas/vila.tscn")
 
 
 func _on_opcoes_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://addons/maaacks_options_menus/base/scenes/menus/options_menu/master_options_menu_with_tabs.tscn")
 
 
 func _on_como_jogar_pressed() -> void:
