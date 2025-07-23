ead6a2c9afe9eb5e055cb499941f1febfa6c05df
valdenicio-jr <valdeniciogame@gmail.com>
2025-07-23 20:07:28 -0300

    atualizações val 23/07

--- tela_inicial.gd
+++ tela_inicial.gd
@@ -1,22 +1,9 @@
 extends Control
 
 
-
-func _on_jogar_pressed() -> void:
-	get_tree().change_scene_to_file("res://cenas/vila.tscn")
-
-
-func _on_opcoes_pressed() -> void:
-	get_tree().change_scene_to_file("res://addons/maaacks_options_menus/base/scenes/menus/options_menu/master_options_menu_with_tabs.tscn")
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/main.tscn")
 
 
-func _on_como_jogar_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_creditos_pressed() -> void:
+func _on_button_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/creditos.tscn")
-
-
-func _on_sair_pressed() -> void:
-	pass # Replace with function body.
